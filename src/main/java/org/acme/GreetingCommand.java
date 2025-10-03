6b3d69844751e93b7d58db3c41ff0cc81aefa7cb
Holly Cummins <holly.k.cummins@googlemail.com>
2025-10-03 13:38:15 +0200

    Basic meme generation

--- src/main/java/org/acme/GreetingCommand.java
+++ src/main/java/org/acme/GreetingCommand.java
@@ -1,19 +1,22 @@
 package org.acme;
 
-import picocli.CommandLine;
+import jakarta.inject.Inject;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "greeting", mixinStandardHelpOptions = true)
 public class GreetingCommand implements Runnable {
+    @Inject
+    MemeService memeService;
 
     @Parameters(paramLabel = "<name>", defaultValue = "picocli",
-        description = "Your name.")
+            description = "Your name.")
     String name;
 
     @Override
     public void run() {
-        System.out.printf("Hello %s, go go commando!%n", name);
+        String path = memeService.generateMeme("hello", "world");
+        System.out.printf("Wrote meme at %s", path);
     }
 
 }
