13cc768c92e34fc201925369d1e7c727b65495f9
Holly Cummins <holly.k.cummins@googlemail.com>
2025-10-04 12:35:29 +0100

    Add support for parameters

---
+++ src/main/java/org/acme/MemeCommand.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import jakarta.inject.Inject;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "meme", mixinStandardHelpOptions = true)
+public class MemeCommand implements Runnable {
+    @Inject
+    MemeService memeService;
+
+    @Parameters(paramLabel = "<top>", defaultValue = "hello",
+            description = "Top text")
+    String top;
+
+    @Parameters(paramLabel = "<bottom>", defaultValue = "world",
+            description = "Bottom text")
+    String bottom;
+
+    @Override
+    public void run() {
+        String path = memeService.generateMeme(top, bottom);
+        System.out.printf("Wrote meme at %s", path);
+    }
+
+}
